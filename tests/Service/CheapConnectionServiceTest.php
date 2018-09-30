3942f205da67b95df52893fa8b0a15fb54039771
Dominik Peuscher <dominik.peuscher@check24.de>
2018-09-30 21:34:43 +0200

    Fix phpunit

--- tests/Service/CheapConnectionServiceTest.php
+++ tests/Service/CheapConnectionServiceTest.php
@@ -6,11 +6,17 @@ use Dpeuscher\BahnSearch\Bahn\FareSearchService;
 use Dpeuscher\BahnSearch\Service\CheapConnectionService;
 use PHPUnit\Framework\TestCase;
 
+/**
+ * Class CheapConnectionServiceTest
+ *
+ * @covers \Dpeuscher\BahnSearch\Service\CheapConnectionService
+ */
 class CheapConnectionServiceTest extends TestCase
 {
 
     public function testGetCheapestConnections()
     {
+        /** @var FareSearchService $fareSearchMock */
         $fareSearchMock = $this->getMockBuilder(FareSearchService::class)->disableOriginalConstructor()->getMock();
         $service = new CheapConnectionService($fareSearchMock);
         $this->assertInstanceOf(CheapConnectionService::class, $service);
